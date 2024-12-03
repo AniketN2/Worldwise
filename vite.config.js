db50917687af344a890aa3ef5ef47f5b78ca314c
AniketN2 <121167260+AniketN2@users.noreply.github.com>
2024-12-03 19:10:41 +0530

    port commit

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import eslint from "vite-plugin-eslint";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
+  server: {
+    port: process.env.PORT || 5173, // Use Render's PORT environment variable or default to 5173
+    host: true, // Listen on all network interfaces (0.0.0.0)
+  }
 });
