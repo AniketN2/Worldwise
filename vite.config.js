4dab6610d98df1012b34ff493359df2f4f360818
AniketN2 <aniket8nalawade@gmail.com>
2025-01-23 21:28:32 +0530

    restart

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,4 @@ import eslint from "vite-plugin-eslint";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
-  server: {
-    port: process.env.PORT || 5173, // Use Render's PORT environment variable or default to 5173
-    host: true, // Listen on all network interfaces (0.0.0.0)
-  }
 });
